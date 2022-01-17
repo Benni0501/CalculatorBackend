a803c8bf6a726c8527beccd32ec0dd23deef8641
Benni0501 <benni.kogler@gmail.com>
2022-01-17 19:51:01 +0100

    alles funktioniert

--- index.js
+++ index.js
@@ -6,8 +6,40 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/login", (req, res) => {
+app.post("/:method", (req, res) => {
+    var numberOne = parseInt(req.body.numberOne);
+    var numberTwo = parseInt(req.body.numberTwo);
+    var result = 0;
+    switch(req.params.method){
+        case "addition":
+            result = numberOne+numberTwo;
+            break;
+        case "subtraktion":
+            result = numberOne-numberTwo;
+            break;
+        case "multiplikation":
+            result = numberOne*numberTwo;
+            break;
+         case "division":
+            if(numberTwo == 0){
+                res.statusCode = 500;
+                result = "Division durch Null nicht möglich!";
+            } else {
+                result = numberOne/numberTwo;
+            }
+            break;
+        default:
+            res.statusCode = 200;
+            break;
+    }
+    var data = {
+        "result":result
+    }
+    console.log(data);
     
+    res.send(JSON.stringify(data));
+    
+    res.end;
 });
 
 console.log("Trying to start server");
